c27e352cce4486490f21217dd05b81cc631e84ec
aaalunchbox <lbox696@gmail.com>
2018-11-25 23:33:04 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Jenjerber',
-        text: 'Hey, this is a people'
-    });
 });
 
 socket.on('disconnect', function () {
